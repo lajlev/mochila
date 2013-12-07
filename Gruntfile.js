b97fd717fcbe42764b93a3d5ba93fea23e673489
Michael Lajlev <lajlev@gmail.com>
2013-12-07 12:01:00 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        files: {
+          'css/app.css': 'sass/app.scss'
+        }
+      }
+    },
+
+    jade: {
+      options: {
+        pretty: true
+      },
+      compile: {
+        files: [{
+          expand: true,
+          cwd: 'views',
+          src: ['*.jade'],
+          dest: '',
+          ext: '.html'
+        }]
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: ''
+        }
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ['sass/**/*.scss','sass/_partials/**/*.scss'],
+        tasks: ['sass:dist']
+      },
+      jade: {
+        files: ['views/**/*.jade'],
+        tasks: ['jade']
+      },
+      livereload: {
+        files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.registerTask('default', ['sass', 'jade', 'connect', 'watch']);
+};
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Starter template for DRY static website node.js
+Comes with
+- Jade
+- Sass
+- Live-reload
+- Compile shit and starts a web server on localhost:9001
+
+@todo
+- Add JS
